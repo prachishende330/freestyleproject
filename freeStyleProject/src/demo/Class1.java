ebd2f585cca6519706bc8bb53db13df2f9932998
prachishende330 <119658404+prachishende330@users.noreply.github.com>
2022-12-02 14:56:31 +0530

    Update Class1.java

--- freeStyleProject/src/demo/Class1.java
+++ freeStyleProject/src/demo/Class1.java
@@ -3,6 +3,7 @@ package demo;
 public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub...");
+		System.out.println("I have succusesfully created my account in GitHub");
 	}
 
 }
